c1960cd21e243134f8f99f48fbafce0a757d89d2
Fitri Awaliyah Muslim <150066815+Antariksaa12@users.noreply.github.com>
2024-06-23 01:32:50 +0700

    update integrasi

--- fe/src/sid/pages/portal/index.jsx
+++ fe/src/sid/pages/portal/index.jsx
@@ -1,4 +1,4 @@
-import react, { Fragment } from 'react'
+import react, {useEffect,useState, Fragment } from 'react'
 import Navbar from '../../../shared/layout/navBar'
 import Footer from '../../../shared/layout/footer'
 import './index.css'
@@ -7,30 +7,27 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Setting from '../../constant/carouselSertting';
 import Image from './assets/fotoportal.png'
+import axios from 'axios';
+const port = import.meta.env.VITE_BASE_API_URL;
 
 const Portal = () => {
-    const data = [
-        {
-            tittle: "SINKAL",
-            image: Image
-        },
-        {
-            tittle: "SID",
-            image: Image
-        },
-        {
-            tittle: "SMART",
-            image: Image
-        },
-        {
-            tittle: "SIKS",
-            image: Image
-        },
-        {
-            tittle: "eHDW",
-            image: Image
+    const [data, setData] = useState([]);
+    const [status, setStatus] = useState('');
+
+    const GetFromAPI = async () => {
+        setStatus('loading')
+        try {
+            const response = await axios.get(`${port}v1/portal/get-portal`);
+            setData(response.data.data);
+            setStatus('success')
+        } catch (error) {
+            console.log(error.message);
+            setStatus('error')
         }
-    ]
+    };
+    useEffect(() => {
+        GetFromAPI();
+        }, []);
     return (
         <Fragment>
             <div className="container-fluid portal-container p-0">
@@ -39,17 +36,17 @@ const Portal = () => {
                     <h1 className='text-center text-light pt-5 pb-2'>Portal Web Pemerintah Daerah</h1>
                     <Slider {...Setting}>
                         {data.map((item, index) => {
-                            // const imageSrc = `http://localhost:3556/upload/${encodeURIComponent(item.img[0])}`;
+                            const imageSrc = `http://localhost:3556/upload/${encodeURIComponent(item.img)}`;
                             return (
                                 <div key={index}  >
                                     <div className="card portal-card m-3 py-4 mx-2" style={{ borderRadius: '1vw', border: '1px solid #00917C', transition: 'transform 0.3s ease' }}>
                                         <div className="row">
                                             <div className="col-1"></div>
                                             <div className="col-10">
-                                                <img src={item.image} alt="" className='mx-auto'/>
-                                                <p>{item.tittle}</p>
+                                                <img src={imageSrc} alt="" className='mx-auto' style={{width:"80%"}}/>
+                                                <p>{item.title}</p>
                                                 <p className='text-dark'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
-                                                <a>Baca lebih banyak</a>
+                                                <a href={item.content}>Baca lebih banyak</a>
                                             </div>
                                             <div className="col-1"></div>
                                         </div>
